be4f92b8c076be7065818dddf1325ba68ef25485
Larry Jones <larryj@blinds.com>
2017-10-21 09:27:03 -0500

    Extract `toDos` reducer

--- 05-basics/src/reducers.js
+++ 05-basics/src/reducers.js
@@ -29,27 +29,45 @@ export const initialState = {
 };
 
 /**
+ * Reduce the list of things to do
+ * @param state The current to do list from the state.
+ * @param action The action to apply to the state.
+ * @returns The new state
+ */
+export function toDos(state = [], action) {
+  switch(action.type) {
+  case ADD_TODO:
+    return [
+      ...state, {
+      text: action.text,
+      completed: false,
+    }];
+    
+  case TOGGLE_TODO:
+    return state.map((toDo, index) => (index === action.index
+        ? Object.assign({}, toDo, { completed: !toDo.completed })
+        : toDo));
+    
+  default:
+    return state;
+  }
+}
+
+/**
  * Our combined (application) reducer.
  * @param state The current application state.
  * @param action The action to apply to the state.
+ * @returns The new state
  */
 export function toDoApp(state = initialState, action) {
   switch(action.type) {
   case SET_VISIBILITY_FILTER:
     return Object.assign({}, state, { visibilityFilter: action.filter });
+    
   case ADD_TODO:
-    return Object.assign({}, state, {
-      toDos: [...state.toDos, {
-        text: action.text,
-        completed: false,
-      }],
-    });
   case TOGGLE_TODO:
-    return Object.assign({}, state, {
-      toDos: state.toDos.map((toDo, index) => (index === action.index
-        ? Object.assign({}, toDo, { completed: true })
-        : toDo))
-    });
+    return Object.assign({}, state, { toDos: toDos(state.toDos, action) });
+    
   default:
     return state;
   }
