da13dec8f824c5e525d242578783cb72952b012c
Larry Jones <larryj@blinds.com>
2017-10-21 09:43:43 -0500

    Use `combineReducers` to create `toDoApp`

--- 05-basics/src/reducers.js
+++ 05-basics/src/reducers.js
@@ -1,8 +1,10 @@
+import { combineReducers } from 'redux';
 import {
   VisibilityFilters,
   ADD_TODO,
   TOGGLE_TODO,
-  SET_VISIBILITY_FILTER } from "./actions";
+  SET_VISIBILITY_FILTER
+} from "./actions";
 
 /**
  *
@@ -34,7 +36,7 @@ export const initialState = {
  * @param action The action to apply to the state.
  * @returns The new state
  */
-export function toDos(state = [], action) {
+function toDos(state = [], action) {
   switch(action.type) {
   case ADD_TODO:
     return [
@@ -54,12 +56,12 @@ export function toDos(state = [], action) {
 }
 
 /**
- * Our visibililty filter reducer.
+ * Our visibility filter reducer.
  * @param state The current visibility filter.
  * @param action The action to apply to the state.
  * @returns The new state
  */
-export function visibility(state = VisibilityFilters.SHOW_ALL, action) {
+function visibilityFilter(state = VisibilityFilters.SHOW_ALL, action) {
   switch(action.type) {
   case SET_VISIBILITY_FILTER:
     return action.filter;
@@ -69,26 +71,7 @@ export function visibility(state = VisibilityFilters.SHOW_ALL, action) {
   }
 }
 
-/**
- * Our combined (application) reducer.
- * @param state The current application state.
- * @param action The action to apply to the state.
- * @returns The new state
- */
-export function toDoApp(state = {}, action) {
-  return {
-    visibilityFilter: visibility(state.visibilityFilter, action),
-    toDos: toDos(state.toDos, action),
-  };
-}
-
-/**
- * Note that each reducer, `toDos`, `visibility` and `toDoApp` handles a *single* part of the
- * application state:
- *
- * - `toDos`: the to do list itself
- * - `visibility`: the visibility filter
- * - `toDoApp`: the single object application statet
- *
- *
- */
+export const toDoApp = combineReducers({
+  visibilityFilter,
+  toDos,
+});
