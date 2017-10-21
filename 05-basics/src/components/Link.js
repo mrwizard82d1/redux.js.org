5d24c3c682e31737936eb8c0f8911363231f8112
Larry Jones <larryj@blinds.com>
2017-10-21 18:32:02 -0500

    Write `Link` component

---
+++ 05-basics/src/components/Link.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+const Link = ({ active, children, onClick }) => {
+  if (active) {
+    return <span>{children}</span>
+  }
+  
+  return ( <a href='#' onClick={(e) => { e.preventDefault(); onClick(); }}>{children}</a>)
+};
+
+Link.propTypes = {
+  active: PropType.bool.isRequired,
+  children: PropType.node.isRequired,
+  onClick: PropType.func.isRequired,
+};
+
+export default Link;
