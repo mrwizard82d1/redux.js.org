1ad2771829e8f87e434b644387ed851987d707a5
Larry Jones <larryj@blinds.com>
2017-10-21 22:11:51 -0500

    Change implementation of `App`
    Include:
    
    - AddToDo
    - VisibleToDoList
    - Footer

--- 05-basics/src/App.js
+++ 05-basics/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddToDo from './containers/AddToDo';
+import VisibleToDoList from './containers/VisibleToDoList';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
@@ -11,7 +14,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <AddToDo />
+          <VisibleToDoList />
+          <Footer />
         </p>
       </div>
     );
