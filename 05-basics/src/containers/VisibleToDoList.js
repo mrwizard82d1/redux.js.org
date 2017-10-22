cb2346e191804a1cdd49c9e924a8eaf266432efc
Larry Jones <larryj@blinds.com>
2017-10-21 21:23:37 -0500

    Write `VisibleToDoList` component

--- 05-basics/src/containers/VisibleToDoList.js
+++ 05-basics/src/containers/VisibleToDoList.js
@@ -1,9 +1,27 @@
 import { connect } from 'react-redux';
+import { toggleToDo, VisibilityFilters } from '../actions';
 import ToDoList from '../components/ToDoList';
 
-const mapStateToProps = (state, ownProps) => ({});
+function visibleToDos(toDos, filter) {
+  switch(filter) {
+  case VisibilityFilters.SHOW_ALL:
+    return toDos;
+    
+  case VisibilityFilters.SHOW_ACTIVE:
+    return toDos.filter(toDo => !toDo.completed);
+    
+  case VisibilityFilters.SHOW_COMPLETED:
+    return toDos.filter(toDo => toDo.completed);
+  }
+}
 
-const mapDispatchToProps = (dispatch, ownProps) => ({});
+const mapStateToProps = (state) => ({
+  toDos: visibleToDos(state.toDos, state.visibilityFilter),
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onToDoClick: id => dispatch(toggleToDo(id)),
+});
 
 const VisibleToDoList = connect(mapStateToProps, mapDispatchToProps)(ToDoList);
 
